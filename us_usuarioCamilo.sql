97ffa2e449054c9507cdf6b5b4c11d3c150245e1
Camilo Castro <castroh24@gmail.com>
2022-10-08 16:52:17 -0600

    Usuario Camilo ingresado y agregado al schema Sales

---
+++ us_usuarioCamilo.sql
@@ -0,0 +1,5 @@
+--4 - Crear un usuario con las iniciales de tus dos nombres + tu primer apellido. Si tu nombre es Felix Ruben García tu login y
+ -- usuario deberían ser frgarcia. Hacer al usuario miembro del rol Vendedores
+
+CREATE USER [cacastro] WITHOUT LOGIN WITH DEFAULT_SCHEMA=[Sales]
+GO
