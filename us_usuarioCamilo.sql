b29226ce1dba59dba7550c8e05c395fb6feaae75
Camilo Castro <castroh24@gmail.com>
2022-10-09 16:36:23 -0600

    Usuario cacastro ingresado al LogInVENDEDORES

--- us_usuarioCamilo.sql
+++ us_usuarioCamilo.sql
@@ -1,5 +1,5 @@
 --4 - Crear un usuario con las iniciales de tus dos nombres + tu primer apellido. Si tu nombre es Felix Ruben García tu login y
  -- usuario deberían ser frgarcia. Hacer al usuario miembro del rol Vendedores
 
-CREATE USER [cacastro] WITHOUT LOGIN WITH DEFAULT_SCHEMA=[Sales]
+CREATE USER [cacastro] FOR LOGIN [userVendedores] WITH DEFAULT_SCHEMA=[dbo]
 GO
