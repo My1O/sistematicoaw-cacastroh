bb86bfb8c7c6a46cbb408c883673eeaf8be9617f
Camilo Castro <castroh24@gmail.com>
2022-10-09 16:31:59 -0600

    Rol vendedores actualizado

--- Rol_Vendedores.sql
+++ Rol_Vendedores.sql
@@ -1,10 +1,21 @@
--- 3 - Crear un nuevo rol llamado Vendedores, los miembros de este rol deberán poseer los permisos de lectura y escritura (actualización y borrado) 
--- sobre el schema Sales y 
--- permisos de lectura sobre las tablas Production. ProductCategory y Production.Product
-
-CREATE ROLE db_Vendedores AUTHORIZATION Production. ProductCategory
+-- 3 - Crear un nuevo rol llamado Vendedores, los miembros de este rol deberán poseer 
+-- los permisos de lectura y escritura (actualización y borrado) sobre el schema Sales y 
+-- permisos de lectura sobre las tablas Production.ProductCategory y Production.Product
+/*
+CREATE LOGIN [userVendedores] WITH PASSWORD='Welcome2022', DEFAULT_DATABASE=[AdventureWorks2019]
 GO
-GRANT SELECT ON SCHEMA::Production.ProductCategory TO db_Sales_ProductCategory_Role
+*/
+CREATE ROLE Vendedores
 GO
-GRANT SELECT ON SCHEMA::Production.Product TO db_Sales_Product_Role
+GRANT INSERT ON SCHEMA::Sales TO Vendedores
+GO
+GRANT UPDATE ON SCHEMA::Sales TO Vendedores
+GO
+GRANT SELECT ON SCHEMA::Sales TO Vendedores
+GO
+GRANT DELETE ON SCHEMA::Sales TO Vendedores
+GO
+GRANT SELECT ON SCHEMA::Production.ProductCategory TO Vendedores
+GO
+GRANT SELECT ON SCHEMA::Production.Product TO Vendedores
 GO
